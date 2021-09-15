0e50a64af1e1281eeab24999f568815b2377b2e4
rafasts <rafa_sts@yahoo.com.br>
2021-09-15 10:14:45 -0300

    inclusão de arquivos

---
+++ Enum/TipoConta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dio.Bank.Enum
+{
+    public enum TipoConta
+    {
+        PessoaFisica = 1,
+        PessoaJuridica = 2
+    }
+}
